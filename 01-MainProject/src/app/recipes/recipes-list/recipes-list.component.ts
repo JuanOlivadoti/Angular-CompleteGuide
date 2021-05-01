3aae5cb4ccf58c06010e3181873f4fed0b40619c
Juan Pablo Olivadoti <juanpablo.olivadoti.contractor@bbva.com>
2021-05-01 21:48:22 +0200

    Passing Data Working

--- 01-MainProject/src/app/recipes/recipes-list/recipes-list.component.ts
+++ 01-MainProject/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { ThisReceiver } from '@angular/compiler';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,7 +9,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-  
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A test recipe', 'This is a description', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'),
     new Recipe('Another test recipe', 'This is another description', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-scotch-quails-eggs-5177019.jpg')
@@ -16,6 +17,11 @@ export class RecipesListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe)
+    
   }
 
 }
