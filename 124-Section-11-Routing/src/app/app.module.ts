fb06a79e1358a80c66ec14580e6ff4e3155d8a1b
Juan Pablo Olivadoti <juanpablo.olivadoti.contractor@bbva.com>
2021-05-15 21:01:55 +0200

    144. Outsourcing the Route Configuration

--- 124-Section-11-Routing/src/app/app.module.ts
+++ 124-Section-11-Routing/src/app/app.module.ts
@@ -1,7 +1,10 @@
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
+// Components
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
@@ -9,31 +12,10 @@ import { ServersComponent } from './servers/servers.component';
 import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
-
-import { ServersService } from './servers/servers.service';
-
-import { RouterModule, Routes } from "@angular/router";
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const appRoutes: Routes = [{
-  path: '', component: HomeComponent
-}, {
-  path: 'users', component: UsersComponent,
-  children: [{
-    path: ':id/:name', component: UserComponent
-  }]
-}, {
-  path: 'servers', component: ServersComponent,
-  children: [{
-    path: ':id', component: ServerComponent
-  }, {
-    path: ':id/edit', component: EditServerComponent
-  }]
-}, {
-  path: 'not-found', component: PageNotFoundComponent
-}, {
-  path: '**', redirectTo: 'not-found'
-}];
+// Services
+import { ServersService } from './servers/servers.service';
 
 @NgModule({
   declarations: [
@@ -49,7 +31,8 @@ const appRoutes: Routes = [{
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
+    // RouterModule.forRoot(appRoutes) Rutas importadas desde AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
