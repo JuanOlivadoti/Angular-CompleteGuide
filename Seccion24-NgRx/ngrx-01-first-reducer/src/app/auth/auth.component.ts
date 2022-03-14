d6bb28394acc7007aeb47eb4f9f16999146a0a4d
Juan Pablo Olivadoti <jpo.workmail@gmail.com>
2022-03-14 09:43:52 +0100

    369 - Updating angular

--- Seccion24-NgRx/ngrx-01-first-reducer/src/app/auth/auth.component.ts
+++ Seccion24-NgRx/ngrx-01-first-reducer/src/app/auth/auth.component.ts
@@ -23,7 +23,7 @@ export class AuthComponent implements OnDestroy {
   isLoginMode = true;
   isLoading = false;
   error: string = null;
-  @ViewChild(PlaceholderDirective, { static: false }) alertHost: PlaceholderDirective;
+  @ViewChild(PlaceholderDirective) alertHost: PlaceholderDirective;
 
   private closeSub: Subscription;
 
